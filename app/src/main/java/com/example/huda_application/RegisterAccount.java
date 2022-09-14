380d9b89e7d70742d7040117ef72b41a463a4a97
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-09-14 14:43:31 -0400

    Generate Java files for XML pages

---
+++ app/src/main/java/com/example/huda_application/RegisterAccount.java
@@ -0,0 +1,14 @@
+package com.example.huda_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_account);
+    }
+}
