da6a59cf3963db04680765f2e5a1e304613e4b87
alibi <ali.bilal.said@hotmail.com>
2022-09-15 13:48:02 -0400

    Implement functionality of Register and forgot password buttons in the main activity.

--- app/src/main/java/com/example/huda_application/MainActivity.java
+++ app/src/main/java/com/example/huda_application/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.huda_application;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,21 +18,33 @@ import com.example.huda_application.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-
+    private TextView registration_button, forgot_pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+        int Registration_ID = R.id.registration;
+        int forgot_password_ID = R.id.forgotpass;
+
+        forgot_pass = (TextView) findViewById(forgot_password_ID);
+        registration_button = (TextView) findViewById(Registration_ID);
 
 
+        registration_button.setOnClickListener(this);
+        forgot_pass.setOnClickListener(this);
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -39,7 +52,21 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.registration){
 
+            Intent Register_Activity = new Intent(this ,RegisterAccount.class);
+            startActivity(Register_Activity);
+
+        }
+        if(view.getId() == R.id.forgotpass){
+
+            Intent password_Activity = new Intent(this ,ForgotPassword.class);
+            startActivity(password_Activity);
+
+        }
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
