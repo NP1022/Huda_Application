a2945f6751fd56e96879f52e3d826e2df3b72d83
alibi <ali.bilal.said@hotmail.com>
2022-09-17 17:03:52 -0400

    Created Main application page with java file and Implemented the User Login on the Main activity java File.

--- app/src/main/java/com/example/huda_application/ForgotPassword.java
+++ app/src/main/java/com/example/huda_application/ForgotPassword.java
@@ -3,12 +3,29 @@ package com.example.huda_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class ForgotPassword extends AppCompatActivity {
+public class ForgotPassword extends AppCompatActivity implements View.OnClickListener{
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+
+
     }
 }
--- app/src/main/java/com/example/huda_application/MainActivity.java
+++ app/src/main/java/com/example/huda_application/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.huda_application;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -15,30 +18,48 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.huda_application.databinding.ActivityMainBinding;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-    private TextView registration_button, forgot_pass;
+    private TextView registration_button, forgot_pass, login;
+    private EditText Password_Text , Email_Text;
+
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
         int Registration_ID = R.id.registration;
         int forgot_password_ID = R.id.forgotpass;
 
+         Password_Text = findViewById(R.id.password);
+         Email_Text = findViewById(R.id.email);
         forgot_pass = (TextView) findViewById(forgot_password_ID);
         registration_button = (TextView) findViewById(Registration_ID);
+        login = (Button) findViewById(R.id.login);
 
-
+        login.setOnClickListener(this);
         registration_button.setOnClickListener(this);
         forgot_pass.setOnClickListener(this);
 
@@ -54,6 +75,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     @Override
     public void onClick(View view) {
+
+        final String emailTxt = Email_Text.getText().toString().trim();
+        final String passwordTxt = Password_Text.getText().toString().trim();
         if(view.getId() == R.id.registration){
 
             Intent Register_Activity = new Intent(this ,RegisterAccount.class);
@@ -66,7 +90,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(password_Activity);
 
         }
+
+        if (view.getId() == R.id.login){
+
+            login(emailTxt, passwordTxt);
+
+        }
+
+
     }
+
+    private void login(String Email, String password) {
+        if (Email.isEmpty()){
+            Email_Text.setError("Email is Empty! ");
+            Email_Text.requestFocus();
+        }
+
+        if (password.isEmpty()){
+            Password_Text.setError("Password is Empty! ");
+            Password_Text.requestFocus();
+        }
+
+        if (password.length() < 6){
+            Password_Text.setError("Password is less than 6 Character! ");
+            Password_Text.requestFocus();
+        }
+        Toast.makeText(MainActivity.this, Email + " " + password, Toast.LENGTH_LONG).show();
+        mAuth.signInWithEmailAndPassword(Email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+
+                    startActivity(new Intent(MainActivity.this , MainApplication.class));
+
+                }
+                else{
+
+
+                    Toast.makeText(MainActivity.this, "Failed To Login! ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -88,4 +156,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+
+
+
 }
