198d1eb9eb44a328b5afd879b31a84dbe50eff7b
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-09-18 21:57:04 -0400

    added email verification, check to if the user verified the email and if they did not then they cannot login to the application

--- app/src/main/java/com/example/huda_application/HomePage.java
+++ app/src/main/java/com/example/huda_application/HomePage.java
@@ -12,18 +12,20 @@ public class HomePage extends AppCompatActivity {
 
 
     FirebaseAuth mAuth; // Added firebase to homepage
+    FirebaseUser mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
         mAuth = FirebaseAuth.getInstance(); // firebase variable to save instance
+        mUser = FirebaseAuth.getInstance().getCurrentUser();
     }
     protected void onStart()
     {
         super.onStart();
         FirebaseUser user = mAuth.getCurrentUser(); // create firebase user
-        if(user == null)
+        if(user == null && user.isEmailVerified()) // check if user is verified
         {
             startActivity(new Intent(HomePage.this, MainActivity.class)); // check if current is logged in or off
         }
