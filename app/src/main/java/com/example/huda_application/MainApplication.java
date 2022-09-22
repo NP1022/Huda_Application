2a97f77505bde50b9e06c74bfd9a38b895272084
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-09-21 23:33:35 -0400

    added verification to check if only verified email accounts can access the MainApplication.java page

--- app/src/main/java/com/example/huda_application/MainApplication.java
+++ app/src/main/java/com/example/huda_application/MainApplication.java
@@ -7,16 +7,28 @@ import android.os.Bundle;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainApplication extends AppCompatActivity
 {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_application);
 
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
 
+        if (user != null && !user.isEmailVerified()) // check if user is verified
+        {
+            startActivity(new Intent(MainApplication.this,MainActivity.class));
+        }
     }
+
 }
