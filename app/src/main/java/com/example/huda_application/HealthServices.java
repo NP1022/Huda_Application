8a0b6dbf967e62d4f43a597780ab9b18d79f922a
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-09-26 17:12:08 -0400

    ScrollView for Health Services

--- app/src/main/java/com/example/huda_application/HealthServices.java
+++ app/src/main/java/com/example/huda_application/HealthServices.java
@@ -3,12 +3,25 @@ package com.example.huda_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HealthServices extends AppCompatActivity {
+    TextView primaryCareHeader, primaryCareText1, primaryCareText2;
+    Button primaryCareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_services);
     }
+    @Override
+    public <T extends View> T findViewById(int id) {
+        return super.findViewById(id);
+    }
+
+    primaryCareHeader = (TextView)findViewById(R.id.primaryCareHeader);
+
+
 }
