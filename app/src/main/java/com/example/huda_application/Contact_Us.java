b4316dffe7122c0001928b5b3093d184a57b4200
Sadia Ahmed <79717373+sahmed586@users.noreply.github.com>
2022-10-23 16:02:41 -0400

    Updated Styling and Linked social medias for the contact us page, created 5 language translations for the contact us page and the our story page

--- app/src/main/java/com/example/huda_application/Contact_Us.java
+++ app/src/main/java/com/example/huda_application/Contact_Us.java
@@ -3,14 +3,19 @@ package com.example.huda_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.FaceDetector;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.rpc.Help;
 
 public class Contact_Us extends AppCompatActivity implements View.OnClickListener {
 
     TextView HUDAnumber, HUDAaddresss;
+    private ImageView Facebook, Twitter, LinkedIn, Instagram;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +23,17 @@ public class Contact_Us extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_contact_us);
         HUDAnumber = (TextView) findViewById(R.id.HUDAnumber);
         HUDAaddresss = (TextView) findViewById(R.id.HUDAaddress);
+        Facebook = (ImageView) findViewById(R.id.facebook);
+        Twitter = (ImageView) findViewById(R.id.twitter);
+        LinkedIn = (ImageView) findViewById(R.id.linkedIn);
+        Instagram = (ImageView) findViewById(R.id.instagram);
 
         HUDAnumber.setOnClickListener((View.OnClickListener) this);
         HUDAaddresss.setOnClickListener((View.OnClickListener) this);
+        Facebook.setOnClickListener((View.OnClickListener) this);
+        Twitter.setOnClickListener((View.OnClickListener) this);
+        LinkedIn.setOnClickListener((View.OnClickListener) this);
+        Instagram.setOnClickListener((View.OnClickListener) this);
     }
 
     @Override
@@ -35,6 +48,24 @@ public class Contact_Us extends AppCompatActivity implements View.OnClickListene
             Intent HUDAMap = new Intent(Intent.ACTION_VIEW, HUDAAddress);
             HUDAMap.setPackage("com.google.android.apps.maps");
             startActivity(HUDAMap);
+        }
+        else if (view.getId() == R.id.facebook)
+        {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/HudaClinic/")));
+
+        }
+        else if (view.getId() == R.id.twitter)
+        {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/hudaclinic")));
+        }
+        else if (view.getId() == R.id.linkedIn)
+        {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/company/hudaclinic/")));
+
+        }
+        else if (view.getId() == R.id.instagram)
+        {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/hudaclinic/")));
         }
     }
 }
