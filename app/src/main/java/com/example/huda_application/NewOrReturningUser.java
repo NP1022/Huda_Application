01b42c85bd2843dad3d48b8d868009af95be649a
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-10-26 18:54:39 -0400

    Added NewOrReturningUser changed redirecting of user account

---
+++ app/src/main/java/com/example/huda_application/NewOrReturningUser.java
@@ -0,0 +1,41 @@
+package com.example.huda_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NewOrReturningUser extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_or_returning_user);
+
+        Button newUserBtn = findViewById(R.id.newUser);
+        Button returningUserBtn = findViewById(R.id.returningUser);
+
+        newUserBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(NewOrReturningUser.this,PatientForm.class));
+            }
+        });
+         returningUserBtn.setOnClickListener(new View.OnClickListener()
+         {
+             @Override
+             public void onClick(View view)
+             {
+                 startActivity(new Intent(NewOrReturningUser.this,MainApplication.class));
+             }
+         });
+
+    }
+}
