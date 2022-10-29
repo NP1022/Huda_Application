6f5c908c06676f4018197ff73d77f683a2235097
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-10-29 14:37:01 -0400

    Vision, Specialty, Mental pages

---
+++ app/src/main/java/com/example/huda_application/VisionHealth.java
@@ -0,0 +1,13 @@
+package com.example.huda_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisionHealth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vision_health);
+    }
+}
