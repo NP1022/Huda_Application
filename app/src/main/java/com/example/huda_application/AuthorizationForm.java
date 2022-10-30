534a42c2bd0c15119b902a8d71510cd164e3e3ce
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-10-29 21:17:39 -0400

    Added if else statements to check the user inputs in the PatientContract form

--- app/src/main/java/com/example/huda_application/AuthorizationForm.java
+++ app/src/main/java/com/example/huda_application/AuthorizationForm.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class AuthorizationForm extends AppCompatActivity implements View.OnClickListener{
+import java.util.regex.Pattern;
+
+public class AuthorizationForm extends AppCompatActivity implements View.OnClickListener
+{
+    private static final Pattern DATE_PATTERN = Pattern.compile(
+            "^((0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|2[0-9])[0-9]{2})$"); // date pattern match
+
+    private static final Pattern SSN_PATTERN = Pattern.compile("^(?!666|000|9\\d{2})\\d{3}"
+            + "-(?!00)\\d{2}-"
+            +"(?!0{4})\\d{4}$");  // SSN pattern match
+
+    private static final Pattern ZIPCODE_PATTERN = Pattern.compile("^\\d{5}$"); // zipcode
+
+    private static final Pattern PHONE_PATTERN = Pattern.compile("^[2-9]\\d{2}-\\d{3}-\\d{4}$"); // pattern to make sure phone numbers are valid format
+
+    private static Pattern PATIENTSEX_PATTERN = Pattern.compile("^(?:m|M|male|Male|f|F|female|Female)$"); // pattern to check patient sex input
+
+    private static Pattern CONTACTPREF_PATTERN = Pattern.compile("^(?:home|Home|mobile|Mobile)$");
+
+    private static Pattern CONSENT_PATTERN = Pattern.compile("^(?:yes|Yes|No|no)$");
+
+    private static Pattern MARITAL_PATTERN = Pattern.compile("^(?:Single|single|Married|married|partner|Partner|Separated|separated|Divorced|divorced|Widowed|widowed)$");
 
     private TextView PersonalRepresentative;
     private String lastname;
@@ -17,13 +41,20 @@ public class AuthorizationForm extends AppCompatActivity implements View.OnClick
             patientEmailTxt,prefLangTxt,translatorTxt,maritalTxt,houseIncomeTxt,houseHoldTxt,occupationTxt,veteranTxt,emergencyNameTxt,relationshipTxt,
             contactPhoneTxt,patientConsentName,patientSignedText,patientSignatureText,consentDateTxt,patientRaceTxt,patientEthnicityTxt,patientIncomeTxt,
             patientEmpTxt,dateTxt,patientSSNTxt;
+
+    final EditText patientName = findViewById(R.id.patientNamePrinted3);
+    final EditText patientSignature = findViewById(R.id.patientNameSignature3);
+    final EditText consentDate = findViewById(R.id.consentFormDate3);
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authorization_form);
         Bundle extras = getIntent().getExtras();
 
-        if (extras != null) {
+        if (extras != null)
+        {
             dateTxt = extras.getString( "dateTxt");
             lastNameTxt = extras.getString( "lastNameTxt");
             firstNameTxt = extras.getString( "firstNameTxt");
@@ -76,45 +107,80 @@ public class AuthorizationForm extends AppCompatActivity implements View.OnClick
     }
 
     @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.nextForm3){
-            Intent PersonalRepresentative = new Intent (this, PersonalRepresentative.class);
-            PersonalRepresentative.putExtra("lastNameTxt" ,lastNameTxt );
-            PersonalRepresentative.putExtra("dateTxt" ,dateTxt );
-            PersonalRepresentative.putExtra("firstNameTxt" ,firstNameTxt );
-            PersonalRepresentative.putExtra("visitReasonTxt" ,visitReasonTxt );
-            PersonalRepresentative.putExtra("patientSexTxt" ,patientSexTxt );
-//AuthorizatPersonalRepresentative
-            PersonalRepresentative.putExtra("patientDOBTxt" ,patientDOBTxt );
-            PersonalRepresentative.putExtra("patientSSNTxt" ,firstNameTxt );
-            PersonalRepresentative.putExtra("patientHomeNumTxt" ,patientHomeNumTxt );
-            PersonalRepresentative.putExtra("patientCellNumTxt" ,patientCellNumTxt );
-            PersonalRepresentative.putExtra("patientAddTxt" ,patientAddTxt );
-            PersonalRepresentative.putExtra("patientCityTxt" ,patientCityTxt );
-            PersonalRepresentative.putExtra("patientStateTxt" ,patientStateTxt );
-            PersonalRepresentative.putExtra("patientZipCodeTxt" ,patientZipCodeTxt );
-            PersonalRepresentative.putExtra("patientPrefNumberTxt" ,patientPrefNumberTxt );
-            PersonalRepresentative.putExtra("patientConsentCallTxt" ,patientConsentCallTxt );
-            PersonalRepresentative.putExtra("patientConsentTextTxt" ,patientConsentTextTxt );
-            PersonalRepresentative.putExtra("patientInsuranceTxt" ,patientInsuranceTxt );
-            PersonalRepresentative.putExtra("patientEmailTxt" ,patientEmailTxt );
-            PersonalRepresentative.putExtra("prefLangTxt" ,prefLangTxt );
-            PersonalRepresentative.putExtra("translatorTxt" ,translatorTxt );
-            PersonalRepresentative.putExtra("maritalTxt" ,maritalTxt );
-            PersonalRepresentative.putExtra("houseIncomeTxt" ,houseIncomeTxt );
-            PersonalRepresentative.putExtra("houseHoldTxt" ,houseHoldTxt );
-            PersonalRepresentative.putExtra("occupationTxt" ,occupationTxt );
-            PersonalRepresentative.putExtra("veteranTxt" ,veteranTxt );
-            PersonalRepresentative.putExtra("emergencyNameTxt" ,emergencyNameTxt );
-            PersonalRepresentative.putExtra("relationshipTxt" ,relationshipTxt );
-            PersonalRepresentative.putExtra("contactPhoneTxt" ,contactPhoneTxt );
-            PersonalRepresentative.putExtra("patientConsentName" ,patientConsentName );
-            PersonalRepresentative.putExtra("patientSignedText" ,patientSignedText );
-            PersonalRepresentative.putExtra("patientSignatureText" ,patientSignatureText );
-            PersonalRepresentative.putExtra("consentDateTxt" ,consentDateTxt );
+    public void onClick(View view)
+    {
+        final String patientNameStr = patientName.getText().toString().trim();
+        final String patientSigStr = patientSignature.getText().toString().trim();
+        final String patientConDateStr = consentDate.getText().toString().trim();
 
-            startActivity(PersonalRepresentative);
+
+        if(patientNameStr.length() > 30 || TextUtils.isEmpty(patientNameStr))
+        {
+            Toast.makeText(AuthorizationForm.this,"Please enter your name",Toast.LENGTH_LONG).show();
+            patientName.setError("Name is required");
+            patientName.requestFocus();
         }
+        else if(patientSigStr.length() > 30 || TextUtils.isEmpty(patientSigStr))
+        {
+            Toast.makeText(AuthorizationForm.this,"Please enter your signature",Toast.LENGTH_LONG).show();
+            patientSignature.setError("Signature is required");
+            patientSignature.requestFocus();
+        }
+        else if(!DATE_PATTERN.matcher(patientConDateStr).matches())
+        {
+            Toast.makeText(AuthorizationForm.this,"Please use MM-DD-YYYY",Toast.LENGTH_LONG).show();
+            consentDate.setError("Format is required");
+            consentDate.requestFocus();
+        }
+        else if(TextUtils.isEmpty(patientConDateStr))
+        {
+            Toast.makeText(AuthorizationForm.this,"Please fill in date",Toast.LENGTH_LONG).show();
+            consentDate.setError("Date is required");
+            consentDate.requestFocus();
+        }
+        else
+        {
+            if (view.getId() == R.id.nextForm3)
+            {
+                //AuthorizatPersonalRepresentative
+                Intent PersonalRepresentative = new Intent (this, PersonalRepresentative.class);
+                PersonalRepresentative.putExtra("lastNameTxt" ,lastNameTxt );
+                PersonalRepresentative.putExtra("dateTxt" ,dateTxt );
+                PersonalRepresentative.putExtra("firstNameTxt" ,firstNameTxt );
+                PersonalRepresentative.putExtra("visitReasonTxt" ,visitReasonTxt );
+                PersonalRepresentative.putExtra("patientSexTxt" ,patientSexTxt );
+                PersonalRepresentative.putExtra("patientDOBTxt" ,patientDOBTxt );
+                PersonalRepresentative.putExtra("patientSSNTxt" ,firstNameTxt );
+                PersonalRepresentative.putExtra("patientHomeNumTxt" ,patientHomeNumTxt );
+                PersonalRepresentative.putExtra("patientCellNumTxt" ,patientCellNumTxt );
+                PersonalRepresentative.putExtra("patientAddTxt" ,patientAddTxt );
+                PersonalRepresentative.putExtra("patientCityTxt" ,patientCityTxt );
+                PersonalRepresentative.putExtra("patientStateTxt" ,patientStateTxt );
+                PersonalRepresentative.putExtra("patientZipCodeTxt" ,patientZipCodeTxt );
+                PersonalRepresentative.putExtra("patientPrefNumberTxt" ,patientPrefNumberTxt );
+                PersonalRepresentative.putExtra("patientConsentCallTxt" ,patientConsentCallTxt );
+                PersonalRepresentative.putExtra("patientConsentTextTxt" ,patientConsentTextTxt );
+                PersonalRepresentative.putExtra("patientInsuranceTxt" ,patientInsuranceTxt );
+                PersonalRepresentative.putExtra("patientEmailTxt" ,patientEmailTxt );
+                PersonalRepresentative.putExtra("prefLangTxt" ,prefLangTxt );
+                PersonalRepresentative.putExtra("translatorTxt" ,translatorTxt );
+                PersonalRepresentative.putExtra("maritalTxt" ,maritalTxt );
+                PersonalRepresentative.putExtra("houseIncomeTxt" ,houseIncomeTxt );
+                PersonalRepresentative.putExtra("houseHoldTxt" ,houseHoldTxt );
+                PersonalRepresentative.putExtra("occupationTxt" ,occupationTxt );
+                PersonalRepresentative.putExtra("veteranTxt" ,veteranTxt );
+                PersonalRepresentative.putExtra("emergencyNameTxt" ,emergencyNameTxt );
+                PersonalRepresentative.putExtra("relationshipTxt" ,relationshipTxt );
+                PersonalRepresentative.putExtra("contactPhoneTxt" ,contactPhoneTxt );
+                PersonalRepresentative.putExtra("patientConsentName" ,patientConsentName );
+                PersonalRepresentative.putExtra("patientSignedText" ,patientSignedText );
+                PersonalRepresentative.putExtra("patientSignatureText" ,patientSignatureText );
+                PersonalRepresentative.putExtra("consentDateTxt" ,consentDateTxt );
+
+                startActivity(PersonalRepresentative);
+            }
+        }
+
 
     }
 }
