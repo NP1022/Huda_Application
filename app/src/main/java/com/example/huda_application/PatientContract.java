b5f0d6c70a9c2411d3ce4917a000855024b91898
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-10-31 14:36:56 -0400

    updated PatientContract Java with input checks

--- app/src/main/java/com/example/huda_application/PatientContract.java
+++ app/src/main/java/com/example/huda_application/PatientContract.java
@@ -4,21 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.huda_application.user.PatientFormData;
 
+import java.util.regex.Pattern;
+
 public class PatientContract extends AppCompatActivity implements View.OnClickListener
 {
+
+    private static final Pattern DATE_PATTERN = Pattern.compile(
+            "^((0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|2[0-9])[0-9]{2})$"); // date pattern match
+
+    final EditText patientSigned = findViewById(R.id.patientNamePrinted2);
+    final EditText patientSig = findViewById(R.id.patientNameSignature2);
+    final EditText consentDateSign = findViewById(R.id.consentFormDate2);
+
+    final String patientSignedText2 = patientSigned.getText().toString().trim();
+    final String patientSignatureText2 = patientSig.getText().toString().trim();
+    final String consentDateTxt2 = consentDateSign.getText().toString().trim();
+
+
     private PatientFormData data;
     private TextView AuthorizationForm;
-    private String visitReasonTxt,firstNameTxt , lastNameTxt,patientSexTxt,patientDOBTxt,patientHomeNumTxt,patientCellNumTxt,patientAddTxt,
-            patientCityTxt,patientStateTxt,patientZipCodeTxt,patientPrefNumberTxt,patientConsentCallTxt,patientConsentTextTxt,patientInsuranceTxt,
-            patientEmailTxt,prefLangTxt,translatorTxt,maritalTxt,houseIncomeTxt,houseHoldTxt,occupationTxt,veteranTxt,emergencyNameTxt,relationshipTxt,
-            contactPhoneTxt,patientConsentName,patientSignedText,patientSignatureText,consentDateTxt,patientRaceTxt,patientEthnicityTxt,patientIncomeTxt,
-            patientEmpTxt,dateTxt,patientSSNTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -40,8 +54,36 @@ public class PatientContract extends AppCompatActivity implements View.OnClickLi
     {
         if (view.getId() == R.id.nextForm2)
         {
-            Intent AuthorizationForm = new Intent (this, AuthorizationForm.class);
-            startActivity(AuthorizationForm);
+            if(TextUtils.isEmpty(patientSignedText2) || patientSignedText2.length() > 30)
+            {
+                Toast.makeText(PatientContract.this, "Name cannot be empty", Toast.LENGTH_LONG).show();
+                patientSigned.setError("Signed name is required");
+                patientSigned.requestFocus();
+            }
+            else if(TextUtils.isEmpty(patientSignatureText2) || patientSignatureText2.length() > 30)
+            {
+                Toast.makeText(PatientContract.this, "Signature cannot be empty", Toast.LENGTH_LONG).show();
+                patientSig.setError("Signature is required");
+                patientSig.requestFocus();
+            }
+            else if(TextUtils.isEmpty(consentDateTxt2))
+            {
+                Toast.makeText(PatientContract.this, "Date cannot be empty", Toast.LENGTH_LONG).show();
+                consentDateSign.setError("Date is required");
+                consentDateSign.requestFocus();
+            }
+            else if(!DATE_PATTERN.matcher(consentDateTxt2).matches())
+            {
+                Toast.makeText(PatientContract.this, "Date format MM-DD-YYYY", Toast.LENGTH_LONG).show();
+                consentDateSign.setError("Date format is required");
+                consentDateSign.requestFocus();
+            }
+            else
+            {
+                Intent AuthorizationForm = new Intent (this, AuthorizationForm.class);
+                startActivity(AuthorizationForm);
+            }
+
         }
     }
 }
