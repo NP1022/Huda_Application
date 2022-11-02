2fb2f0865134e050e799981c90437eb159802124
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-11-02 15:47:30 -0400

    Check In timer

--- app/src/main/java/com/example/huda_application/CheckIn.java
+++ app/src/main/java/com/example/huda_application/CheckIn.java
@@ -1,10 +1,15 @@
 package com.example.huda_application;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +18,9 @@ import com.example.huda_application.user.Appointment;
 import com.example.huda_application.user.AppointmentStatus;
 import com.example.huda_application.user.UserManager;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.regex.Pattern;
 
 public class CheckIn extends AppCompatActivity implements View.OnClickListener {
@@ -21,9 +29,10 @@ public class CheckIn extends AppCompatActivity implements View.OnClickListener {
             "^((0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|2[0-9])[0-9]{2})$"); // date pattern match
 
     private static final Pattern TIME_PATTERN = Pattern.compile("(1[012]|[1-9]):[0-5][0-9](\\s)?(?i)(am|pm)");
-
+    private EditText apptTime;
     private EditText date, birthday, reason, time;
     private Button checkin;
+    int t2Hour, t2Minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,18 +44,20 @@ public class CheckIn extends AppCompatActivity implements View.OnClickListener {
         reason = findViewById(R.id.email_checkin);
         time = findViewById(R.id.appointmentTime);
         checkin = findViewById(R.id.checkin);
+        apptTime = findViewById(R.id.appointmentTime);
 
         date.setOnClickListener(this);
         birthday.setOnClickListener(this);
         reason.setOnClickListener(this);
         time.setOnClickListener(this);
         checkin.setOnClickListener(this);
+        apptTime.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.checkin) {
-            
+
             Appointment appointment = new Appointment(
                     time.getText().toString(),
                     date.getText().toString(),
@@ -58,6 +69,35 @@ public class CheckIn extends AppCompatActivity implements View.OnClickListener {
             FirebaseClient.updateUser(UserManager.getInstance().getCurrentUser());
             Intent appointmentsIntent = new Intent(this, Appointments.class);
             startActivity(appointmentsIntent);
+        }
+        if (view.getId() == R.id.appointmentTime) {
+            TimePickerDialog apttimePicker = new TimePickerDialog(
+                    CheckIn.this,
+                    android.R.style.Theme_Holo_Dialog_MinWidth,
+                    new TimePickerDialog.OnTimeSetListener() {
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                            t2Hour = hourOfDay;
+                            t2Minute = minute;
+                            String time = t2Hour + ":" + t2Minute;
+                            SimpleDateFormat f24Hours = new SimpleDateFormat(
+                                    "HH:mm"
+                            );
+                            try {
+                                Date date = f24Hours.parse(time);
+                                SimpleDateFormat f12Hours = new SimpleDateFormat(
+                                        "hh:mm aa"
+                                );
+                                apptTime.setText(f12Hours.format(date));
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, 12, 0, false
+            );
+            apttimePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            apttimePicker.updateTime(t2Hour, t2Minute);
+            apttimePicker.show();
         }
     }
 }
