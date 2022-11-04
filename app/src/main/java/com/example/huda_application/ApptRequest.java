153812a6644e30d7b4892a28a3a311b0fbc0fe52
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-11-03 22:48:17 -0400

    Merge branch 'Masrur_development' of /Users/masrurtajwar/Documents/GitHub/Huda_Application with conflicts.

--- app/src/main/java/com/example/huda_application/ApptRequest.java
+++ app/src/main/java/com/example/huda_application/ApptRequest.java
@@ -2,13 +2,10 @@ package com.example.huda_application;
 
 import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,46 +14,39 @@ import com.example.huda_application.user.Appointment;
 import com.example.huda_application.user.AppointmentStatus;
 import com.example.huda_application.user.UserManager;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.regex.Pattern;
 
-public class ApptRequest extends AppCompatActivity implements View.OnClickListener {
+public class CheckIn extends AppCompatActivity implements View.OnClickListener {
 
     private static final Pattern DATE_PATTERN = Pattern.compile(
             "^((0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-(19|2[0-9])[0-9]{2})$"); // date pattern match
-
+    private t2Hour, t2Minute; 
     private static final Pattern TIME_PATTERN = Pattern.compile("(1[012]|[1-9]):[0-5][0-9](\\s)?(?i)(am|pm)");
-    private EditText apptTime;
     private EditText date, birthday, reason, time;
     private Button checkin;
-    int t2Hour, t2Minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appt_request);
+        setContentView(R.layout.activity_check_in);
 
         date = findViewById(R.id.FullName);
         birthday = findViewById(R.id.birthday);
         reason = findViewById(R.id.email_checkin);
         time = findViewById(R.id.appointmentTime);
         checkin = findViewById(R.id.checkin);
-        apptTime = findViewById(R.id.appointmentTime);
 
         date.setOnClickListener(this);
         birthday.setOnClickListener(this);
         reason.setOnClickListener(this);
         time.setOnClickListener(this);
         checkin.setOnClickListener(this);
-        apptTime.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.checkin) {
-
+            
             Appointment appointment = new Appointment(
                     time.getText().toString(),
                     date.getText().toString(),
@@ -69,9 +59,9 @@ public class ApptRequest extends AppCompatActivity implements View.OnClickListen
             Intent appointmentsIntent = new Intent(this, Appointments.class);
             startActivity(appointmentsIntent);
         }
-        if (view.getId() == R.id.appointmentTime) {
+        else if (view.getId() == R.id.appointmentTime) {
             TimePickerDialog apttimePicker = new TimePickerDialog(
-                    ApptRequest.this,
+                    CheckIn.this,
                     android.R.style.Theme_Holo_Dialog_MinWidth,
                     new TimePickerDialog.OnTimeSetListener() {
                         @Override
@@ -97,6 +87,8 @@ public class ApptRequest extends AppCompatActivity implements View.OnClickListen
             apttimePicker.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             apttimePicker.updateTime(t2Hour, t2Minute);
             apttimePicker.show();
-        }
+        });
+
+    }
     }
 }
