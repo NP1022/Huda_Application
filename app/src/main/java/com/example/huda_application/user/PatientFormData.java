b0d9afccdbe8cc85a4658787355920f183b9b8e1
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-11-05 00:19:42 -0400

    Added New Strings to PatientFormData class

--- app/src/main/java/com/example/huda_application/user/PatientFormData.java
+++ app/src/main/java/com/example/huda_application/user/PatientFormData.java
@@ -41,6 +41,422 @@ public class PatientFormData implements Serializable
     private String patientNamePrinted;
     private String patientSignature;
     private String patientConsentDate;
+    // Form 2 Strings
+    private String consentDateForm2;
+    private String patientSignedForm2;
+    private String patientSignatureForm2;
+    // Form 3 Strings
+    private String consentDateForm3;
+    private String patientSignedForm3;
+    private String patientSignatureForm3;
+    // Form 4 strings
+    private String patientSignedForm4;
+    private String dateOfBirthForm4;
+    private String homeAddressForm4;
+    private String cityForm4;
+    private String stateForm4;
+    private String zipCodeForm4;
+    private String phoneNumberForm4;
+    private String personalRep1;
+    private String relationship1;
+    private String personalRepPhone1;
+    private String personalRep2;
+    private String relationship2;
+    private String personalRepPhone2;
+    private String personalRep3;
+    private String relationship3;
+    private String personalRepPhone3;
+    private String patientSignatureForm4;
+    private String consentDateForm4;
+    // Form 5 Strings
+    private String question1Section1Form5;
+    private String question2Section1Form5;
+    private String question3Section1Form5;
+    private String question4Section1Form5;
+    private String question5Section1Form5;
+    private String question6Section1Form5;
+    private String question7Section1Form5;
+    private String question8Section1Form5;
+    private String section1TotalsForm5;
+    private String question1Section2Form5;
+    private String question2Section2Form5;
+    private String question3Section2Form5;
+    private String question4Section2Form5;
+    private String question5Section2Form5;
+    private String question6Section2Form5;
+    private String question7Section2Form5;
+    private String section2TotalsForm5;
+    private String question1Section3Form5;
+    private String question2Section3Form5;
+    private String question3Section3Form5;
+    private String question4Section3Form5;
+
+
+
+    public String getQuestion1Section1Form5() {
+        return question1Section1Form5;
+    }
+
+    public void setQuestion1Section1Form5(String question1Section1Form5) {
+        this.question1Section1Form5 = question1Section1Form5;
+    }
+
+    public String getQuestion2Section1Form5() {
+        return question2Section1Form5;
+    }
+
+    public void setQuestion2Section1Form5(String question2Section1Form5) {
+        this.question2Section1Form5 = question2Section1Form5;
+    }
+
+    public String getQuestion3Section1Form5() {
+        return question3Section1Form5;
+    }
+
+    public void setQuestion3Section1Form5(String question3Section1Form5) {
+        this.question3Section1Form5 = question3Section1Form5;
+    }
+
+    public String getQuestion4Section1Form5() {
+        return question4Section1Form5;
+    }
+
+    public void setQuestion4Section1Form5(String question4Section1Form5) {
+        this.question4Section1Form5 = question4Section1Form5;
+    }
+
+    public String getQuestion5Section1Form5() {
+        return question5Section1Form5;
+    }
+
+    public void setQuestion5Section1Form5(String question5Section1Form5) {
+        this.question5Section1Form5 = question5Section1Form5;
+    }
+
+    public String getQuestion6Section1Form5() {
+        return question6Section1Form5;
+    }
+
+    public void setQuestion6Section1Form5(String question6Section1Form5) {
+        this.question6Section1Form5 = question6Section1Form5;
+    }
+
+    public String getQuestion7Section1Form5() {
+        return question7Section1Form5;
+    }
+
+    public void setQuestion7Section1Form5(String question7Section1Form5) {
+        this.question7Section1Form5 = question7Section1Form5;
+    }
+
+    public String getQuestion8Section1Form5() {
+        return question8Section1Form5;
+    }
+
+    public void setQuestion8Section1Form5(String question8Section1Form5) {
+        this.question8Section1Form5 = question8Section1Form5;
+    }
+
+    public String getSection1TotalsForm5() {
+        return section1TotalsForm5;
+    }
+
+    public void setSection1TotalsForm5(String section1TotalsForm5) {
+        this.section1TotalsForm5 = section1TotalsForm5;
+    }
+
+    public String getQuestion1Section2Form5() {
+        return question1Section2Form5;
+    }
+
+    public void setQuestion1Section2Form5(String question1Section2Form5) {
+        this.question1Section2Form5 = question1Section2Form5;
+    }
+
+    public String getQuestion2Section2Form5() {
+        return question2Section2Form5;
+    }
+
+    public void setQuestion2Section2Form5(String question2Section2Form5) {
+        this.question2Section2Form5 = question2Section2Form5;
+    }
+
+    public String getQuestion3Section2Form5() {
+        return question3Section2Form5;
+    }
+
+    public void setQuestion3Section2Form5(String question3Section2Form5) {
+        this.question3Section2Form5 = question3Section2Form5;
+    }
+
+    public String getQuestion4Section2Form5() {
+        return question4Section2Form5;
+    }
+
+    public void setQuestion4Section2Form5(String question4Section2Form5) {
+        this.question4Section2Form5 = question4Section2Form5;
+    }
+
+    public String getQuestion5Section2Form5() {
+        return question5Section2Form5;
+    }
+
+    public void setQuestion5Section2Form5(String question5Section2Form5) {
+        this.question5Section2Form5 = question5Section2Form5;
+    }
+
+    public String getQuestion6Section2Form5() {
+        return question6Section2Form5;
+    }
+
+    public void setQuestion6Section2Form5(String question6Section2Form5) {
+        this.question6Section2Form5 = question6Section2Form5;
+    }
+
+    public String getQuestion7Section2Form5() {
+        return question7Section2Form5;
+    }
+
+    public void setQuestion7Section2Form5(String question7Section2Form5) {
+        this.question7Section2Form5 = question7Section2Form5;
+    }
+
+    public String getSection2TotalsForm5() {
+        return section2TotalsForm5;
+    }
+
+    public void setSection2TotalsForm5(String section2TotalsForm5) {
+        this.section2TotalsForm5 = section2TotalsForm5;
+    }
+
+    public String getQuestion1Section3Form5() {
+        return question1Section3Form5;
+    }
+
+    public void setQuestion1Section3Form5(String question1Section3Form5) {
+        this.question1Section3Form5 = question1Section3Form5;
+    }
+
+    public String getQuestion2Section3Form5() {
+        return question2Section3Form5;
+    }
+
+    public void setQuestion2Section3Form5(String question2Section3Form5) {
+        this.question2Section3Form5 = question2Section3Form5;
+    }
+
+    public String getQuestion3Section3Form5() {
+        return question3Section3Form5;
+    }
+
+    public void setQuestion3Section3Form5(String question3Section3Form5) {
+        this.question3Section3Form5 = question3Section3Form5;
+    }
+
+    public String getQuestion4Section3Form5() {
+        return question4Section3Form5;
+    }
+
+    public void setQuestion4Section3Form5(String question4Section3Form5) {
+        this.question4Section3Form5 = question4Section3Form5;
+    }
+
+
+
+    public String getPatientSignedForm4() {
+        return patientSignedForm4;
+    }
+
+    public void setPatientSignedForm4(String patientSignedForm4) {
+        this.patientSignedForm4 = patientSignedForm4;
+    }
+
+    public String getDateOfBirthForm4() {
+        return dateOfBirthForm4;
+    }
+
+    public void setDateOfBirthForm4(String dateOfBirthForm4) {
+        this.dateOfBirthForm4 = dateOfBirthForm4;
+    }
+
+    public String getHomeAddressForm4() {
+        return homeAddressForm4;
+    }
+
+    public void setHomeAddressForm4(String homeAddressForm4) {
+        this.homeAddressForm4 = homeAddressForm4;
+    }
+
+    public String getCityForm4() {
+        return cityForm4;
+    }
+
+    public void setCityForm4(String cityForm4) {
+        this.cityForm4 = cityForm4;
+    }
+
+    public String getStateForm4() {
+        return stateForm4;
+    }
+
+    public void setStateForm4(String stateForm4) {
+        this.stateForm4 = stateForm4;
+    }
+
+    public String getZipCodeForm4() {
+        return zipCodeForm4;
+    }
+
+    public void setZipCodeForm4(String zipCodeForm4) {
+        this.zipCodeForm4 = zipCodeForm4;
+    }
+
+    public String getPhoneNumberForm4() {
+        return phoneNumberForm4;
+    }
+
+    public void setPhoneNumberForm4(String phoneNumberForm4) {
+        this.phoneNumberForm4 = phoneNumberForm4;
+    }
+
+    public String getPersonalRep1() {
+        return personalRep1;
+    }
+
+    public void setPersonalRep1(String personalRep1) {
+        this.personalRep1 = personalRep1;
+    }
+
+    public String getRelationship1() {
+        return relationship1;
+    }
+
+    public void setRelationship1(String relationship1) {
+        this.relationship1 = relationship1;
+    }
+
+    public String getPersonalRepPhone1() {
+        return personalRepPhone1;
+    }
+
+    public void setPersonalRepPhone1(String personalRepPhone1) {
+        this.personalRepPhone1 = personalRepPhone1;
+    }
+
+    public String getPersonalRep2() {
+        return personalRep2;
+    }
+
+    public void setPersonalRep2(String personalRep2) {
+        this.personalRep2 = personalRep2;
+    }
+
+    public String getRelationship2() {
+        return relationship2;
+    }
+
+    public void setRelationship2(String relationship2) {
+        this.relationship2 = relationship2;
+    }
+
+    public String getPersonalRepPhone2() {
+        return personalRepPhone2;
+    }
+
+    public void setPersonalRepPhone2(String personalRepPhone2) {
+        this.personalRepPhone2 = personalRepPhone2;
+    }
+
+    public String getPersonalRep3() {
+        return personalRep3;
+    }
+
+    public void setPersonalRep3(String personalRep3) {
+        this.personalRep3 = personalRep3;
+    }
+
+    public String getRelationship3() {
+        return relationship3;
+    }
+
+    public void setRelationship3(String relationship3) {
+        this.relationship3 = relationship3;
+    }
+
+    public String getPersonalRepPhone3() {
+        return personalRepPhone3;
+    }
+
+    public void setPersonalRepPhone3(String personalRepPhone3) {
+        this.personalRepPhone3 = personalRepPhone3;
+    }
+
+    public String getPatientSignatureForm4() {
+        return patientSignatureForm4;
+    }
+
+    public void setPatientSignatureForm4(String patientSignatureForm4) {
+        this.patientSignatureForm4 = patientSignatureForm4;
+    }
+
+    public String getConsentDateForm4() {
+        return consentDateForm4;
+    }
+
+    public void setConsentDateForm4(String consentDateForm4) {
+        this.consentDateForm4 = consentDateForm4;
+    }
+
+
+    public String getConsentDateForm3() {
+        return consentDateForm3;
+    }
+
+    public void setConsentDateForm3(String consentDateForm3) {
+        this.consentDateForm3 = consentDateForm3;
+    }
+
+    public String getPatientSignedForm3() {
+        return patientSignedForm3;
+    }
+
+    public void setPatientSignedForm3(String patientSignedForm3) {
+        this.patientSignedForm3 = patientSignedForm3;
+    }
+
+    public String getPatientSignatureForm3() {
+        return patientSignatureForm3;
+    }
+
+    public void setPatientSignatureForm3(String patientSignatureForm3) {
+        this.patientSignatureForm3 = patientSignatureForm3;
+    }
+
+
+
+    public String getConsentDateForm2() {
+        return consentDateForm2;
+    }
+
+    public void setConsentDateForm2(String consentDateForm2) {
+        this.consentDateForm2 = consentDateForm2;
+    }
+
+    public String getPatientSignedForm2() {
+        return patientSignedForm2;
+    }
+
+    public void setPatientSignedForm2(String patientSignedForm2) {
+        this.patientSignedForm2 = patientSignedForm2;
+    }
+
+    public String getPatientSignatureForm2() {
+        return patientSignatureForm2;
+    }
+
+    public void setPatientSignatureForm2(String patientSignatureForm2) {
+        this.patientSignatureForm2 = patientSignatureForm2;
+    }
 
     public String getUID() {
         return UID;
@@ -345,13 +761,29 @@ public class PatientFormData implements Serializable
 
     }
 
-    public PatientFormData(String todaysDate, String reasonForVisit, String lastName, String firstName, String patientSex, String dateOfBirth, String patientAddress,
-                           String patientCity, String patientState, String patientZipCode, String patientSSN, String homePhoneNo, String cellPhoneNo, String prefContactMethod,
-                           String consentToCall, String consentToText, String insuranceProvider, String emailAddress, String patientRace, String patientEthnicity,
+    public PatientFormData(String todaysDate, String reasonForVisit, String lastName, String firstName, String patientSex, String dateOfBirth,
+                           String patientAddress,
+                           String patientCity, String patientState, String patientZipCode, String patientSSN, String homePhoneNo, String cellPhoneNo,
+                           String prefContactMethod,
+                           String consentToCall, String consentToText, String insuranceProvider, String emailAddress, String patientRace,
+                           String patientEthnicity,
                            String prefLanguage, String translatorNeed, String maritalStatus, String houseHoldIncome, String incomePayTime, String familySize,
                            String employmentStatus, String occupation, String veteranStatus, String emergencyContact, String emergencyRelationship,
                            String emergencyContactNumber, String patientNameSelfVolunteer, String patientNamePrinted, String patientSignature,
-                           String patientConsentDate)
+                           String patientConsentDate,String consentDateForm2,String patientSignedForm2,String patientSignatureForm2,
+                           String consentDateForm3,String patientSignedForm3,String patientSignatureForm3,String patientSignedForm4, String dateOfBirthForm4,
+                           String homeAddressForm4, String cityForm4,String stateForm4, String zipCodeForm4, String phoneNumberForm4, String personalRep1,
+                           String relationship1, String personalRepPhone1, String personalRep2, String relationship2, String personalRepPhone2,
+                           String personalRep3, String relationship3, String personalRepPhone3,String patientSignatureForm4, String consentDateForm4,
+                           String question1Section1Form5, String question2Section1Form5, String question3Section1Form5, String question4Section1Form5,
+                           String question5Section1Form5, String question6Section1Form5, String question7Section1Form5, String question8Section1Form5,
+                           String section1TotalsForm5, String question1Section2Form5, String question2Section2Form5, String question3Section2Form5,
+                           String question4Section2Form5, String question5Section2Form5, String question6Section2Form5, String question7Section2Form5,
+                           String section2TotalsForm5, String question1Section3Form5, String question2Section3Form5, String question3Section3Form5,
+                           String question4Section3Form5
+
+
+                           )
     {
         this.todaysDate = todaysDate;
         this.reasonForVisit = reasonForVisit;
@@ -389,6 +821,52 @@ public class PatientFormData implements Serializable
         this.patientNamePrinted = patientNamePrinted;
         this.patientSignature = patientSignature;
         this.patientConsentDate = patientConsentDate;
+        this.consentDateForm2 = consentDateForm2;
+        this.patientSignedForm2 = patientSignedForm2;
+        this.patientSignatureForm2 = patientSignatureForm2;
+        this.consentDateForm3 = consentDateForm3;
+        this.patientSignedForm3 = patientSignedForm3;
+        this.patientSignatureForm3 = patientSignatureForm3;
+        this.patientSignedForm4 = patientSignedForm4;
+        this.dateOfBirthForm4 = dateOfBirthForm4;
+        this.homeAddressForm4 = homeAddressForm4;
+        this.cityForm4 = cityForm4;
+        this.stateForm4 = stateForm4;
+        this.zipCodeForm4 = zipCodeForm4;
+        this.phoneNumberForm4 = phoneNumberForm4;
+        this.personalRep1 = personalRep1;
+        this.relationship1 = relationship1;
+        this.personalRepPhone1 = personalRepPhone1;
+        this.personalRep2 = personalRep2;
+        this.relationship2 = relationship2;
+        this.personalRepPhone2 = personalRepPhone2;
+        this.personalRep3 = personalRep3;
+        this.relationship3 = relationship3;
+        this.personalRepPhone3 = personalRepPhone3;
+        this.patientSignatureForm4 = patientSignatureForm4;
+        this.consentDateForm4 = consentDateForm4;
+        this.question1Section1Form5 = question1Section1Form5;
+        this.question2Section1Form5 = question2Section1Form5;
+        this.question3Section1Form5 = question3Section1Form5;
+        this.question4Section1Form5 = question4Section1Form5;
+        this.question5Section1Form5 = question5Section1Form5;
+        this.question6Section1Form5 = question6Section1Form5;
+        this.question7Section1Form5 = question7Section1Form5;
+        this.question8Section1Form5 = question8Section1Form5;
+        this.section1TotalsForm5 = section1TotalsForm5;
+        this.question1Section2Form5 = question1Section2Form5;
+        this.question2Section2Form5 = question2Section2Form5;
+        this.question3Section2Form5 = question3Section2Form5;
+        this.question4Section2Form5 = question4Section2Form5;
+        this.question5Section2Form5 = question5Section2Form5;
+        this.question6Section2Form5 = question6Section2Form5;
+        this.question7Section2Form5 = question7Section2Form5;
+        this.section2TotalsForm5 = section2TotalsForm5;
+        this.question1Section3Form5 = question1Section3Form5;
+        this.question2Section3Form5 = question2Section3Form5;
+        this.question3Section3Form5 = question3Section3Form5;
+        this.question4Section3Form5 = question4Section3Form5;
+
     }
 
 
@@ -398,3 +876,4 @@ public class PatientFormData implements Serializable
 //    public PatientFormData() {
 //        this("", "", "");
 //    }
+
