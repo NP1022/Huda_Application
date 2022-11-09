64671176f3588f97dcae6968aaf2008109f6c144
Sadia Ahmed <79717373+sahmed586@users.noreply.github.com>
2022-11-09 10:57:49 -0500

    Implemented the calendar
    This commit includes both the styled Google calendar that is displayed on the page as well as the code for the local android studio calendar.

--- app/src/main/java/com/example/huda_application/Announcements.java
+++ app/src/main/java/com/example/huda_application/Announcements.java
@@ -15,64 +15,92 @@ import android.widget.TextView;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class Announcements extends AppCompatActivity implements CalAdapter.OnItemListener
-{
-    private TextView monthYearText;
-    private RecyclerView calRecycler;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_announcements);
-        initializeWidgets();
-    }
 
-    private void initializeWidgets()
-    {
-        calRecycler = findViewById(R.id.calRecycler);
-        monthYearText = findViewById(R.id.monthYearTV);
-        CalUtilities.selectedDate = LocalDate.now();
-        setMonthView();
-    }
+public class Announcements extends AppCompatActivity {
 
-    private void setMonthView()
-    {
-        monthYearText.setText(monthYearFromDate(CalUtilities.selectedDate));
-        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalUtilities.selectedDate);
-
-        CalAdapter calAdapter = new CalAdapter(daysInMonth, this);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
-        calRecycler.setLayoutManager(layoutManager);
-        calRecycler.setAdapter(calAdapter);
-    }
-
-    public void navigatePrevMonth(View view)
-    {
-        // goes back one month
-        CalUtilities.selectedDate = CalUtilities.selectedDate.minusMonths(1);
-        setMonthView();
-    }
-
-    public void navigateNextMonth(View view)
-    {
-        // forward one month
-        CalUtilities.selectedDate = CalUtilities.selectedDate.plusMonths(1);
-        setMonthView();
-    }
+    // web view var
+    private WebView webView;
 
     @Override
-    //toast message when item is clicked
-    // change this
-    public void onItemClick(int position, String dayText)
-    {
-        if(dayText.equals(""))
-        {
-            String message = "Selected Date" + dayText + " " + monthYearFromDate(CalUtilities.selectedDate);
-        }
-    }
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_announcements);
 
-    public void weeklyView(View view)
-    {
-        startActivity(new Intent(this, weeklyCalendar.class));
+        webView = findViewById(R.id.calendarWebView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // <iframe src=\"https://calendar.google.com/calendar/embed?src=c_tns32v5e01q5kjs1jj1rcuehao%40group.calendar.google.com&ctz=America%2FDetroit\" style=\"border: 0\" width=\"390\" height=\"350\" frameborder=\"0\" scrolling=\"no\"></iframe>"
+        //String googleHTML = "<iframe src=\"https://embed.styledcalendar.com/#my4zA7wGz2FkThs3z3eJ\" title=\"Styled Calendar\" class=\"styled-calendar-container\" style=\"width= 390px; height=\"350\" border: none;\" data-cy=\"calendar-embed-iframe\"></iframe>";
+        //String googleHTML = "<iframe src=\"https://calendar.google.com/calendar/embed?src=c_tns32v5e01q5kjs1jj1rcuehao%40group.calendar.google.com&ctz=America%2FDetroit\" style=\"width=\"390\" height=\"350\"></iframe>";
+        String googleHTML = "https://embed.styledcalendar.com/#my4zA7wGz2FkThs3z3eJ";
+        webView.loadUrl(googleHTML);
     }
 }
+
+//public class Announcements extends AppCompatActivity
+//{
+//    private TextView monthYearText;
+//    private RecyclerView calRecycler;
+//
+//    protected void onCreate(Bundle savedInstanceState)
+//    {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_announcements);
+//        //initializeWidgets();
+//    }
+
+//    private void initializeWidgets()
+//    {
+//        calRecycler = findViewById(R.id.calRecycler);
+//        monthYearText = findViewById(R.id.monthYearTV);
+//        CalUtilities.selectedDate = LocalDate.now();
+//        setMonthView();
+//    }
+//
+//    private void setMonthView()
+//    {
+//        monthYearText.setText(monthYearFromDate(CalUtilities.selectedDate));
+//        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalUtilities.selectedDate);
+//
+//        CalAdapter calAdapter = new CalAdapter(daysInMonth, this);
+//        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+//        calRecycler.setLayoutManager(layoutManager);
+//        calRecycler.setAdapter(calAdapter);
+//    }
+//
+//    public void navigatePrevMonth(View view)
+//    {
+//        // goes back one month
+//        CalUtilities.selectedDate = CalUtilities.selectedDate.minusMonths(1);
+//        setMonthView();
+//    }
+//
+//    public void navigateNextMonth(View view)
+//    {
+//        // forward one month
+//        CalUtilities.selectedDate = CalUtilities.selectedDate.plusMonths(1);
+//        setMonthView();
+//    }
+//
+//    @Override
+//    //toast message when item is clicked
+//    // change this
+//    public void onItemClick(int position, LocalDate date)
+//    {
+//        if(date != null)
+//        {
+//            CalUtilities.selectedDate = date;
+//            setMonthView();
+//        }
+//    }
+//
+//    public void weeklyView(View view)
+//    {
+//        startActivity(new Intent(this, WeeklyCal.class));
+//    }
+
+//}
