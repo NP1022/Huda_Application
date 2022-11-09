e3acedce707ad5df53f1eb6823b4a862c3ba35af
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-11-09 17:55:46 -0500

    Backend profile page data transfer

--- app/src/main/java/com/example/huda_application/ProfilePage.java
+++ app/src/main/java/com/example/huda_application/ProfilePage.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.example.huda_application.firebase.FirebaseClient;
 import com.example.huda_application.user.User;
+import com.example.huda_application.user.UserManager;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,46 +19,26 @@ import com.google.firebase.database.ValueEventListener;
 public class ProfilePage extends AppCompatActivity {
     private TextView firstName;
     private TextView email;
-    private TextView lastname;
+    private TextView lastName;
+    private TextView DOB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
 
-        Intent intent = getIntent();
-        User user = (User) intent.getSerializableExtra("user");
-
         this.firstName = findViewById(R.id.et_first_name);
+        this.lastName = findViewById(R.id.et_last_name);
+        this.email = findViewById(R.id.et_email);
+        this.DOB = findViewById(R.id.et_date_of_birth);
 
+        User user = UserManager.getInstance().getCurrentUser();
         firstName.setText(String.format("%s", user.getFirstName()));
+        lastName.setText(String.format("%s", user.getLastName()));
+        email.setText(String.format("%s", user.getEmailAddress()));
+        DOB.setText(String.format("%s", user.getBirthday()));
 
-        FirebaseDatabase.getInstance().getReference("User").child(user.getUserId()).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // User updatedUser = FirebaseClient.convertToUser(snapshot);
-                User user = snapshot.getValue(User.class);
-                String userFirstName = user.getFirstName();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-            }
-        });
-
-        this.lastname = findViewById(R.id.last_name);
-        this.email = findViewById(R.id.email);
     }
 
-        public TextView getFirstName() {
-            return firstName;
-        }
 
-        public TextView getEmail() {
-            return email;
-        }
-
-        public TextView getLastname() {
-            return lastname;
-        }
 }
