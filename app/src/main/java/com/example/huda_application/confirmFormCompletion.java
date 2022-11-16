ed7e1f39608d3368988aa87b417fc9059208e47b
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-11-16 11:28:03 -0500

    Updated UI for Update Email

---
+++ app/src/main/java/com/example/huda_application/confirmFormCompletion.java
@@ -0,0 +1,32 @@
+package com.example.huda_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class confirmFormCompletion extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_form_completion);
+
+        Button redirectUser = findViewById(R.id.login); // set variable for button action
+
+        redirectUser.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(confirmFormCompletion.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
