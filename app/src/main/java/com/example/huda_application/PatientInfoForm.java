d5290ff4de78ed270aac7051c6a056fe2a2c48f2
syedh99 <73625090+syedh99@users.noreply.github.com>
2022-11-27 19:43:08 -0500

    Added comments to the PatientInfoForm.java

--- app/src/main/java/com/example/huda_application/PatientInfoForm.java
+++ app/src/main/java/com/example/huda_application/PatientInfoForm.java
@@ -20,17 +20,23 @@ public class PatientInfoForm extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_info_form);
         Intent extras = getIntent();
+
+        // if statement to check if the extras are null, if it is null
+        // then set data to the extras.getSerializableExtra();
         if (extras != null)
         {
             data =  (PatientFormData)  extras.getSerializableExtra("patientdata");
         }
-        // Log.i("info  ", "The user name in the application is   " + lastNameTxt + firstNameTxt );
+
         LabAccess = (TextView) findViewById(R.id.nextForm6);
         LabAccess.setOnClickListener(this);
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View view)
+    {
+        // if user hits nextForm6 button, then they will be taken to the next form
+        // no inputs on this page
         if (view.getId() == R.id.nextForm6)
         {
             Intent LabAccess = new Intent(this, LabAccess.class);
