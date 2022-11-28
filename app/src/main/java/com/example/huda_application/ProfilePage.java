f424074210b81b8fcb45a0b97b6c4081a21057f3
Masrur Tajwar <79229270+masrurtajwar@users.noreply.github.com>
2022-11-28 18:20:19 -0500

    Profile Page completed comments

--- app/src/main/java/com/example/huda_application/ProfilePage.java
+++ app/src/main/java/com/example/huda_application/ProfilePage.java
@@ -38,46 +38,52 @@ public class ProfilePage extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_profile_page);
 
         header = findViewById(R.id.tv_heading); // Finding header View ID from associated XML
-        firstName = findViewById(R.id.name); // Finding first name TextView ID from associated XML
-        email = findViewById(R.id.et_email); // Finding email TextView ID from associated XML
-        DOB = findViewById(R.id.et_date_of_birth); // Finding date of birth TextView ID from associated XML
-        updateEmail = (FloatingActionButton) findViewById(R.id.floatingEmail); // Finding update email Button ID from associated XML
-
-        deleteAcc = findViewById(R.id.deleteUserButton); // 
+        firstName = findViewById(R.id.name);
+        email = findViewById(R.id.et_email);
+        DOB = findViewById(R.id.et_date_of_birth);
+        updateEmail = (FloatingActionButton) findViewById(R.id.floatingEmail);
+        deleteAcc = findViewById(R.id.deleteUserButton);
         signOut = findViewById(R.id.logoutButton);
         updatePass = findViewById(R.id.updatePassword);
         backbutton = findViewById(R.id.backButton_10);
-        backbutton.setOnClickListener(this);
+
+        backbutton.setOnClickListener(this); // Wiring all variables to View.OnClickListener in order to click on
         signOut.setOnClickListener(this);
         deleteAcc.setOnClickListener(this);
         updateEmail.setOnClickListener(this);
         updatePass.setOnClickListener(this);
 
-        User user = UserManager.getInstance().getCurrentUser();
-        header.setText(String.format("%s", "Welcome, " + user.getFirstName() + "!"));
-        firstName.setText(String.format("%s", user.getFirstName() + " " + user.getLastName()));
+        User user = UserManager.getInstance().getCurrentUser(); // Fetch data of current user object
+        header.setText(String.format("%s", "Welcome, " + user.getFirstName() + "!")); // Set header text to welcome the user, fetching user's first name from database
+        firstName.setText(String.format("%s", user.getFirstName() + " " + user.getLastName())); // Sets name string to display user's first and last name
         // lastName.setText(String.format("%s", user.getLastName()));
-        email.setText(String.format("%s", user.getEmailAddress()));
-        DOB.setText(String.format("%s", user.getBirthday()));
+        email.setText(String.format("%s", user.getEmailAddress())); // Sets email string to display user's email address
+        DOB.setText(String.format("%s", user.getBirthday())); // Sets date of birth string to display user's date of birth
 
         mAuth = FirebaseAuth.getInstance();
-        FirebaseUser fbUser = mAuth.getCurrentUser();
+        FirebaseUser fbUser = mAuth.getCurrentUser(); // Get current user authorized
     }
 
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.backButton_10) {
-            startActivity(new Intent(this, MainApplication.class));
-        } else if (v.getId() == R.id.logoutButton) {
-            UserManager.getInstance().removeCurrentUser();
-            mAuth.signOut();
-            startActivity(new Intent(this, MainActivity.class));
-        } else if (v.getId() == R.id.updatePassword) {
-            startActivity(new Intent(this, updatePassword.class));
-        } else if (v.getId() == R.id.floatingEmail) {
-            startActivity(new Intent(this, updateEmail.class));
-        } else if (v.getId() == R.id.deleteUserButton) {
-            startActivity(new Intent(this, deleteUserAccount.class));
+    public void onClick(View v) // onClick function to respond to user input of click, redirecting user
+    {
+        if (v.getId() == R.id.backButton_10) // If user clicks on back button
+        {
+            startActivity(new Intent(this, MainApplication.class)); // User is redirected to previous page, set to return to Main Menu
+        } else if (v.getId() == R.id.logoutButton) // If user clicks on logout button
+        {
+            UserManager.getInstance().removeCurrentUser(); // User's instance is removed
+            mAuth.signOut(); // Firebase Authenticator signs the user out
+            startActivity(new Intent(this, MainActivity.class)); // The user is redirected to the account login page, after signing out
+        } else if (v.getId() == R.id.updatePassword) // If the user clicks on the update password button
+        {
+            startActivity(new Intent(this, updatePassword.class)); // The user is redirected to the update password page
+        } else if (v.getId() == R.id.floatingEmail) // If the user clicks on the update email function
+        {
+            startActivity(new Intent(this, updateEmail.class)); // The user is redirected to the update email page
+        } else if (v.getId() == R.id.deleteUserButton) // If the user clicks on the delete account button
+        {
+            startActivity(new Intent(this, deleteUserAccount.class)); // The user is redirected to the delete account page
         }
     }
 }
