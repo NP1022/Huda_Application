2c2ef53cd8a7867483ff81ee8d852358b401ad57
alibi <ali.bilal.said@hotmail.com>
2022-11-29 17:58:54 -0500

    Deleted unused health services file and commented on new login java

--- app/src/main/java/com/example/huda_application/NewLogin.java
+++ app/src/main/java/com/example/huda_application/NewLogin.java
@@ -32,7 +32,8 @@ public class NewLogin extends AppCompatActivity implements View.OnClickListener 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_login);
-
+                                                                                // Class that will show the Main page of the application with the buttons that
+                                                                                // show the health services for the clinic
         login = (TextView) findViewById(R.id.LoginButton);
         signUp = (TextView) findViewById(R.id.SignUpButton);
 
@@ -43,7 +44,7 @@ public class NewLogin extends AppCompatActivity implements View.OnClickListener 
         MentalHealth_Button = (TextView) findViewById(R.id.MentalHeader);
         VisionCare_Button = (TextView) findViewById(R.id.VisionHeader);
         DentalCare_Button = (TextView) findViewById(R.id.DentalHeader);
-        SpecialtyCare_Button = (TextView) findViewById(R.id.SpecialtyHeader);
+        SpecialtyCare_Button = (TextView) findViewById(R.id.SpecialtyHeader);               //The buttons that are used for the specific health Service for the application
         PrimaryCare_Button.setOnClickListener(this);
         MentalHealth_Button.setOnClickListener(this);
         VisionCare_Button.setOnClickListener(this);
@@ -56,7 +57,7 @@ public class NewLogin extends AppCompatActivity implements View.OnClickListener 
         language_Button = findViewById(R.id.language2);
         language_Button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) {                                                    // Used to change the language in the application
 
                 options.setSingleChoiceItems(langs_options, -1, new DialogInterface.OnClickListener() {
                     @Override
@@ -115,7 +116,7 @@ public class NewLogin extends AppCompatActivity implements View.OnClickListener 
     {
         SharedPreferences.Editor Saver = getSharedPreferences("langauge", MODE_MULTI_PROCESS).edit();
         Locale language_option =  new Locale(l);
-        DisplayMetrics metrics =  getBaseContext().getResources().getDisplayMetrics();
+        DisplayMetrics metrics =  getBaseContext().getResources().getDisplayMetrics();                  // Picks the locale after the language is picked from the dialog
         language_swtich(l, metrics, language_option);
 
         Saver.putString("prev_language" ,l);
@@ -130,7 +131,7 @@ public class NewLogin extends AppCompatActivity implements View.OnClickListener 
 
     public void Saved_language(){
         SharedPreferences saved_language =getSharedPreferences("langauge", MODE_MULTI_PROCESS);
-        picklanguage(saved_language.getString("prev_language" , ""));
+        picklanguage(saved_language.getString("prev_language" , ""));           // choose the saved language from the application
     }
 
 
@@ -139,7 +140,7 @@ public class NewLogin extends AppCompatActivity implements View.OnClickListener 
 
         Locale.setDefault(lang);
 
-        Configuration page = new Configuration();
+        Configuration page = new Configuration();                                                   // Switch the language
         page.locale = lang;
 
         getBaseContext().getResources().updateConfiguration(page, m);
@@ -157,7 +158,7 @@ public class NewLogin extends AppCompatActivity implements View.OnClickListener 
         else if (view.getId() == R.id.PrimaryHeader) {
             Intent Primary = new Intent(this, PrimaryServices.class);
             startActivity(Primary);
-        }
+        }                                                                           // On click for all the buttons that are located on the page of the application
         else if (view.getId() == R.id.MentalHeader) {
             Intent Mental = new Intent(this, MentalHealth.class);
             startActivity(Mental);
